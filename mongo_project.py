5deeb923c9ed18c9d728f090864252251a01b78b
Alexandru Apostoiu <alexander.apostoiu@gmail.com>
2020-06-08 12:07:58 +0000

    created add and get functions for the menu

--- mongo_project.py
+++ mongo_project.py
@@ -4,7 +4,7 @@ import os
 if os.path.exists("env.py"):
     import env
 
-MONGODB_URI = os.getenv("MONGO_URI")
+MONGODB_URI = os.getenv('MONGODB_URI')
 DBS_NAME = "myTestDB"
 COLLECTION_NAME = "myFirstMDB"
 
@@ -16,6 +16,23 @@ def mongo_connect(url):
         return conn
     except pymongo.errors.ConnectionFailure as e:
         print("Could not connect to MongoDB: %s") % e
+
+
+def get_record():
+    print("")
+    first = input("Enter first name > ")
+    last = input("Enter last name > ")
+
+    try:
+        doc = coll.find_one({'first': first.lower(), 'last': last.lower()})
+    except:
+        print("Error accessing the database")
+
+    if not doc:
+        print("")
+        print("Error! No results found.")
+
+    return doc
 
 
 def show_menu():
@@ -30,11 +47,33 @@ def show_menu():
     return option
 
 
+def add_record():
+    print("")
+    first = input("Enter first name > ")
+    last = input("Enter last name > ")
+    dob = input("Enter date of birth > ")
+    gender = input("Enter gender > ")
+    hair_colour = input("Enter hair colour > ")
+    occupation = input("Enter occupation > ")
+    nationality = input("Enter nationality > ")
+
+    new_doc = {'first': first.lower(), 'last': last.lower(), 'dob': dob,
+               'gender': gender, 'hair_colour': hair_colour, 'occupation':
+               occupation, 'nationality': nationality}
+ 
+    try:
+        coll.insert_one(new_doc)
+        print("")
+        print("Document inserted")
+    except:
+        print("Error accessing the database")
+
+
 def main_loop():
     while True:
         option = show_menu()
         if option == "1":
-            print("You have selected option 1")
+            add_record()
         elif option == "2":
             print("You have selected option 2")
         elif option == "3":
@@ -50,6 +89,7 @@ def main_loop():
 
 
 conn = mongo_connect(MONGODB_URI)
+
 coll = conn[DBS_NAME][COLLECTION_NAME]
 
 main_loop()
