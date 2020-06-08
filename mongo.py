28519be4b6b2f12975abd288c5f059840b7a74f1
Alexandru Apostoiu <alexander.apostoiu@gmail.com>
2020-06-08 11:35:47 +0000

    delete_one method

--- mongo.py
+++ mongo.py
@@ -24,13 +24,7 @@ conn = mongo_connect(MONGODB_URI)
 
 coll = conn[DBS_NAME][COLLECTION_NAME]
 
-new_docs = [{'first': 'terry', 'last': 'pratchett', 'dob': '28/04/1948',
-           'gender': 'm', 'hair_colour': 'not much', 'occupation': 'writer', 
-           'nationality': 'english'}, {'first': 'george', 'last': 'rr martin',
-           'dob': '20/09/1948', 'gender': 'm', 'hair_colour': 'white',
-           'occupation': 'writer', 'nationality': 'american'}]
-
-coll.insert_many(new_docs)
+coll.delete_one({'first': 'george'})
 
 documents = coll.find()
 
