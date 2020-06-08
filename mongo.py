d6e82bdb9f23c011c3bac0b5a117c4ef42b8020a
Alexandru Apostoiu <alexander.apostoiu@gmail.com>
2020-06-08 10:47:16 +0000

    fixed paths

--- mongo.py
+++ mongo.py
@@ -1,11 +1,12 @@
 import pymongo
 import os
 
+
 if os.path.exists("env.py"):
     import env
 
 
-MONGODB_URI = os.getenv("MONGO_URI")
+MONGODB_URI = os.getenv('MONGODB_URI')
 DBS_NAME = "myTestDB"
 COLLECTION_NAME = "myFirstMDB"
 
